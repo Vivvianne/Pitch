dfebf2ac8401c74b8abf7f3b4496883eed54b306
Vivvianne Kimani <ndiahav@gmail.com>
2019-06-29 20:52:22 +0300

    add sensitive data in start.sh and gonfig.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     SQLACHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_MODIFICATIONS = 'postgresql+psycopg2://nkimani:her1234@localhost/pitch'
     
-    SECRET_KEY = "I'm not boarding"
+    SECRET_KEY = 'Im not boarding'
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
