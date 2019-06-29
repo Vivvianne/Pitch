eaf814c54d6996702476c15890b0b6911d92bcf5
Vivvianne Kimani <ndiahav@gmail.com>
2019-06-29 21:54:41 +0300

    installed flask-migrate, flask-wtf, flask-mail, flask-uploads, flask-script and initialized the db

--- config.py
+++ config.py
@@ -7,20 +7,21 @@ class Config:
     
     SECRET_KEY = 'Im not boarding'
     MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     
-    class ProdConfig(Config):
+class ProdConfig(Config):
         SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://nkimani:her1234@localhost/pitch'
         SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
         
         
-    class DevConfig(Config):
+class DevConfig(Config):
         DEBUG = True
         
         
-    config_options = {
+config_options = {
     'development': DevConfig,
     'production': ProdConfig
 }
