e231c1ac443878d1a35b7f87e5bce47d8a96c72a
Vivvianne Kimani <ndiahav@gmail.com>
2019-06-30 21:31:38 +0300

    changed the interpreter insettings.json and changed the instances in the amke_shell_context function from entries to pitch

--- manage.py
+++ manage.py
@@ -26,7 +26,7 @@ def test():
 
 @manager.shell
 def make_shell_context():
-    return dict(app=app, db=db, User=User_pitch, Pitch = Entries, Comments=Comments, PitchCategory=Category)
+    return dict(app=app, db=db, User=User_pitch, Pitch = Pitch, Comments=Comments, PitchCategory=PitchCategory)
     pass
 
 
