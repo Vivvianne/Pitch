070c2348d981cf955e88a0fd6094226a77ac8f69
Vivvianne Kimani <ndiahav@gmail.com>
2019-07-02 12:50:19 +0300

    update untracking of files

--- start.sh
+++
@@ -1,6 +0,0 @@
-export SECRET_KEY='Im not boarding'
-export SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://nkimani:her1234@localhost/pitch'
-export MAIL_USERNAME='ndiahav@gmail.com'
-export MAIL_PASSWORD='her1234'
-
-python3.6 manage.py server
